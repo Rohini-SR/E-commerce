bc58b24ee55ed488b2d6e1e9bb61024c6bcea999
Rohini-SR <srohinics@gmail.com>
2024-09-23 19:50:24 +0530

    backend files added

--- back-end/routes/productRoute.js
+++ back-end/routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { listProducts, addProduct, removeProduct, singleProduct } from '../controllers/productController.js'
+import adminAuth from '../middleware/adminAuth.js';
 
 
 const productRouter = express. Router();
-productRouter.post('/add',upload.fields([{name:image1,maxcount:1}, {name:image2,maxcount:1},{name:image3,maxcount:1},{name:image4,maxcount:1}]), addProduct);
-productRouter.post("/remove", removeProduct);
+productRouter.post('/add',adminAuth,upload.fields([{name:image1,maxcount:1}, {name:image2,maxcount:1},{name:image3,maxcount:1},{name:image4,maxcount:1}]), addProduct);
+productRouter.post("/remove",adminAuth, removeProduct);
 productRouter.post('/single', singleProduct);
 productRouter.get('/list',listProducts)
 
